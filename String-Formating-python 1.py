13fc0f645e9984f381aa110c3be2d3d00fefd2f8
Salar Issa <120121093+salarissa321@users.noreply.github.com>
2023-04-19 13:06:15 +0200

    Update String-Formating-python 1.py

--- String-Formating-python 1.py
+++ String-Formating-python 1.py
@@ -9,12 +9,12 @@ age= 36
 rank = 10
 
 
-print("My Name is : " + name)
-# print("My Name is : " + name + "and my Age is :" + age)
+print("My Name is : " + name) # My Name is : salar
+print("My Name is : " + name + "and my Age is :" + age) # TypeError: can only concatenate str (not "int") to str
 
-print("My Name is %s" % "salar")
-print("My Name is %s and my Age is : %d" % (name,age) )
-print("My Name is %s and my Age is : %d and my rank is : %f " % (name,age, rank) )
+print("My Name is %s" % "salar") # My Name is salar
+print("My Name is %s and my Age is : %d" % (name,age) ) #My Name is salar and my Age is : 36
+print("My Name is %s and my Age is : %d and my rank is : %f " % (name,age, rank) ) # My Name is salar and my Age is : 36 and my rank is : 10.000000
 
 # %s => String
 # %d => Integer
@@ -24,14 +24,14 @@ n= "Salar"
 l="python"
 y=20
 
-print("My Name is %s and i am %s developer with %d Years Exp" % (n,l,y))
+print("My Name is %s and i am %s developer with %d Years Exp" % (n,l,y)) # My Name is Salar and i am python developer with 20 Years Exp
 
 # control Floating Point Number
 myNumber = 9
-print("my Number is: %d" % myNumber)
-print("my Number is: %f" % myNumber)
-print("my Number is: %.2f" % myNumber)
+print("my Number is: %d" % myNumber) # my Number is: 9
+print("my Number is: %f" % myNumber) # my Number is: 9.000000
+print("my Number is: %.2f" % myNumber) # my Number is: 9.00
 
 # Truncate String
 myLongString ="Hello guys how are you what are you doing now"
-print("Message is %.5s" % myLongString)
+print("Message is %.5s" % myLongString) # Message is Hello
